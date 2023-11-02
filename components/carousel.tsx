2b9f844aab318880555f45888e3e15de36379a1d
Calvin <calvinducharme@gmail.com>
2023-11-01 19:51:04 -0700

    trial

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -4,7 +4,7 @@ import { GridTileImage } from './grid/tile';
 
 export async function Carousel() {
   // Collections that start with `hidden-*` are hidden from the search page.
-  const products = await getCollectionProducts({ collection: 'hidden-homepage-carousel' });
+  const products = await getCollectionProducts({ collection: 'the-timeless-collection' });
 
   if (!products?.length) return null;
 
@@ -17,9 +17,9 @@ export async function Carousel() {
         {carouselProducts.map((product, i) => (
           <li
             key={`${product.handle}${i}`}
-            className="relative aspect-square h-[30vh] max-h-[275px] w-2/3 max-w-[475px] flex-none md:w-1/3"
+            className="relative aspect-square h-[30vh] max-h-[275px] w-fit max-w-[475px] flex-none md:w-1/3"
           >
-            <Link href={`/product/${product.handle}`} className="relative h-full w-full">
+            <Link href={`/product/${product.handle}`} className="relative h-full w-fit">
               <GridTileImage
                 alt={product.title}
                 label={{
